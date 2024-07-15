3837b88ef87e55f22aafef84e03a6e5d2d27799f
cassandraGoose <cassandra.torske@gmail.com>
2024-07-15 14:09:55 -0600

    remove unnecessary import

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import { Navbar, Button, Menu } from 'react-daisyui';
+import { Navbar, Menu } from 'react-daisyui';
 import { Link } from 'react-router-dom';
 
 export default function Navigation() {
